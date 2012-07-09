1d550ee14a2f0c6c84249bfa629d908fc0810461
Ian-Huang <dk781020@hotmail.com>
2012-07-09 22:03:57 +0800

    Update Accelerate & Jump scripts

--- Assets/Game mechanism/AccelerateMechanism.cs
+++ Assets/Game mechanism/AccelerateMechanism.cs
@@ -4,21 +4,17 @@ using System.Collections;
 public class AccelerateMechanism : MonoBehaviour
 {
     public GameObject Greta;
-    private GretaController control;
-    public float SpeedUp = 8;
+    public float SpeedUp = 10;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.name == this.Greta.name)
-        {
-            this.control.Velocity = this.SpeedUp;
-        }
+        Greta.rigidbody.velocity = Greta.transform.TransformDirection(0, 0, SpeedUp);
+        Greta.rigidbody.AddForce(Greta.transform.TransformDirection(0, 0, SpeedUp));
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         this.Greta = GameObject.Find("Greta");
-        this.control = this.Greta.GetComponent<GretaController>();
-	}
-	
+    }
 }
--- Assets/Game mechanism/JumpMechanism.cs
+++ Assets/Game mechanism/JumpMechanism.cs
@@ -5,28 +5,15 @@ public class JumpMechanism : MonoBehaviour
 {
     public float JumpSpeed = 1;    
     public GameObject Greta;
-    private GretaController control;
 
     void OnTriggerEnter(Collider other)
-    {
-        if (other.name == this.Greta.name)
-        {
-            this.control.SetJumpSpeed(JumpSpeed);
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.name == Greta.name)
-        {
-            this.control.SetJumpSpeed(0);
-        }
-    }
+    {        
+        Greta.rigidbody.AddForce(transform.TransformDirection(0, 0, JumpSpeed));
+    }    
 
     // Use this for initialization
 	void Start () 
     {
         this.Greta = GameObject.Find("Greta");
-        this.control = this.Greta.GetComponent<GretaController>();
 	}	
 }
