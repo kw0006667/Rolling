d4a41e0d87e7aa1137106fff1947beda282f66b8
Tim Chang <kw0006667@hotmail.com>
2012-07-09 14:26:09 +0800

    Update WheelController.cs
    Fixed RigidBody addFoce function.

--- Assets/Script/WheelController.cs
+++ Assets/Script/WheelController.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class WheelController : MonoBehaviour {
+public class WheelController : MonoBehaviour
+{
 
     public WheelCollider LeftWheel;
     public WheelCollider RightWheel;
@@ -18,64 +19,59 @@ public class WheelController : MonoBehaviour {
     private Rigidbody wheelRigidbody;
 
     private float turnValue;
+    private float tempTorque;
+    private float forceTonque;
+    private float brakeTonque;
+    private float powerTonque;
 
     private bool isHandBreak;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
-        //this.rigidbody.centerOfMass = new Vector3(0.0f, 0.0f, 0.0f);
-
-        // Set the rigidbody center point to lower
-        //this.wheelRigidbody.centerOfMass = new Vector3(-0.1f, 0.0f, 0.0f);
         this.rigidbody.centerOfMass = this.CenterOfMass.localPosition;
 
-        this.turnValue = 0;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-    {
-        float tempTorque = this.MaxTonque * Input.GetAxis("Vertical");
+        this.turnValue = 0.0f;
+        this.tempTorque = 0.0f;
+        this.forceTonque = 0.0f;
+        this.brakeTonque = 0.0f;
+        this.powerTonque = 0.0f;
+    }
 
-        if (!isHandBreak)
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (this.tempTorque != 0.0f)
         {
-            //this.rigidbody.AddForce(this.transform.TransformDirection(Vector3.forward) * Time.deltaTime * tempTorque * this.MaxTonque);
-            this.LeftWheel.motorTorque = tempTorque * this.MaxTonque;
-            this.RightWheel.motorTorque = tempTorque * this.MaxTonque;
+            this.forceTonque += this.tempTorque;
+            if (this.forceTonque >= this.MaxTonque)
+                this.forceTonque = this.MaxTonque;
+            this.forceTonque += this.tempTorque;
         }
         else
         {
-            tempTorque = 0;
-            //this.rigidbody.AddForce(this.transform.TransformDirection(Vector3.forward) * Time.deltaTime * tempTorque * this.MaxTonque);
-            this.rigidbody.velocity = Vector3.zero;
-            this.LeftWheel.motorTorque = 0;
-            this.RightWheel.motorTorque = 0;
-            this.LeftWheel_Front.motorTorque = 0;
-            this.RightWheel_Front.motorTorque = 0;
+            this.forceTonque = 0.0f;
+            //if (this.forceTonque == 0.0f)
+            //    this.forceTonque = 0.0f;
+        }
+
+        if (!isHandBreak)
+        {
+            this.rigidbody.AddForce(this.transform.TransformDirection(Vector3.forward) * (this.forceTonque));
+        }
+        else
+        {
+            // Verify the velocity of rigidbody is not zero
+            if (!Vector3.Equals(this.rigidbody.velocity.normalized, Vector3.zero))
+            {
+                this.rigidbody.AddForce(this.transform.TransformDirection(Vector3.forward) * (this.brakeTonque) * 2.0f);
+                //this.rigidbody.velocity = Vector3.zero;
+            }
         }
 
         Debug.DrawRay(this.transform.position, this.transform.TransformDirection(Vector3.forward), Color.red);
 
-        //this.turnValue = Input.GetAxis("Horizontal");
-
-        //if (this.MouseX_Value < 0)
-        //{
-        //    this.LeftWheel.motorTorque += this.MaxTonque * this.MouseX_Value;
-        //    //this.RightWheel.motorTorque += 10.0f;
-        //}
-        //else if (this.MouseX_Value > 0)
-        //{
-        //    //this.LeftWheel.motorTorque = 50.0f;
-        //    this.RightWheel.motorTorque += this.MaxTonque * this.MouseX_Value;
-        //}
-
-        
-
-        //this.leftWheel.steerAngle = -30 * Input.GetAxis("Horizontal");
-        //this.rightWheel.steerAngle = 30 * Input.GetAxis("Horizontal");
-        
-	}
+    }
 
     void Update()
     {
@@ -83,13 +79,18 @@ public class WheelController : MonoBehaviour {
         this.VectorY = this.rigidbody.worldCenterOfMass.y;
         this.VectorZ = this.rigidbody.worldCenterOfMass.z;
 
+        tempTorque = Input.GetAxis("Vertical");
         this.MouseX_Value = Input.GetAxis("Mouse X");
 
         if (this.MouseX_Value >= 5)
             this.MouseX_Value = 5;
 
         if (Input.GetMouseButtonDown(2))
+        {
+            if (!this.isHandBreak)
+                this.brakeTonque = -this.forceTonque;
             this.isHandBreak = true;
+        }
         if (Input.GetMouseButtonUp(2))
             this.isHandBreak = false;
 
