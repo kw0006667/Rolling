bbfdce3e905e31a4812f794ef95fbea9d3018c78
Tim Chang <kw0006667@hotmail.com>
2012-07-13 19:16:43 +0800

    Add Camera Collision Detection
    Via Raycast

--- Assets/Script/SmoothFollow.cs
+++ Assets/Script/SmoothFollow.cs
@@ -14,19 +14,20 @@ public class SmoothFollow : MonoBehaviour {
     public float RotationDamping = 3.0f;
 
     public Vector3 forward = new Vector3(0.0f, 1.0f, 0.0f);
-    public Vector3 localPoint;
-    //public LayerMask LineOfSightMask = 0;
+    public LayerMask LineOfSightMask = 0;
 
-    //public float CloserRadius = 0.2f;
-    //public float CloserSnapLag = 0.2f;
+    public float CloserRadius = 0.2f;
+    public float CloserSnapLag = 0.2f;
 
-    //private float currentDistance = 10.0f;
-    //private float distanceVelocity = 0.0f;
+    private float currentDistance = 10.0f;
+    private float distanceVelocity = 0.0f;
 
 	// Use this for initialization
 	void Start () 
     {
         //this.currentDistance = this.Distance;
+        if (this.rigidbody)
+            this.rigidbody.freezeRotation = true;
 	}
 	
 	// Update is called once per frame
@@ -98,63 +99,33 @@ public class SmoothFollow : MonoBehaviour {
             // Set the height of the camera
             this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, currentHeight);
 
-            this.localPoint = this.transform.TransformPoint(transform.position);
-            //-------------------------------
-
-            // Calculate the current rotation angles
-            //float wanteddRotationAngle = this.Target.eulerAngles.y;
-            //float wantedHeight = this.Target.position.y + this.Height;
-
-            //float currentRotationAngle = this.transform.eulerAngles.y;
-            //float currentHeight = this.transform.position.y;
-
-            //// Damp the rotation around the y-axis
-            //currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wanteddRotationAngle, this.RotationDamping * Time.deltaTime);
-
-            //// Damp the height
-            //currentHeight = Mathf.Lerp(currentHeight, wantedHeight, this.HeightDamping * Time.deltaTime);
-
-            //// Convert the angle into a rotation
-            //Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
-
-            //// Set the position of the camera on the x-z plane to:
-            //this.transform.position = this.Target.position;
-            //this.transform.position -= currentRotation * Vector3.forward * this.Distance;
-
-            //// Set the height of the camera
-            //this.transform.position = new Vector3(this.transform.position.x, currentHeight, this.transform.position.z);
-
-            //Quaternion rotation = Quaternion.LookRotation(this.transform.position - this.Target.position);
-            //rotation = Quaternion.LookRotation(this.Target.position - this.transform.position, new Vector3(0.0f, 0.0f, -1.0f));
-            //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.deltaTime* this.RotationDamping * 20);
-
             // Always look at the target
             this.transform.LookAt(this.Target, new Vector3(0.0f, 0.0f, -1.0f));
         }
 
-        //// Adjust Camera position when raycast the wall
-        //Vector3 targetPos = this.transform.position + this.transform.TransformDirection(Vector3.forward);
-        //float targetDistance = this.adjustLineOfSight(ref targetPos, this.transform.TransformDirection(-Vector3.forward));
-        //this.currentDistance = Mathf.SmoothDamp(this.currentDistance, targetDistance, ref this.distanceVelocity, this.CloserSnapLag * 0.3f);
+        // Adjust Camera position when raycast the wall
+        Vector3 targetPos = this.Target.transform.position;
+        float targetDistance = this.adjustLineOfSight(ref targetPos, Vector3.Normalize(this.transform.position - this.Target.position));
+        this.currentDistance = Mathf.SmoothDamp(this.currentDistance, targetDistance, ref this.distanceVelocity, this.CloserSnapLag * 1.0f);
 
-        //this.transform.position = targetPos + this.transform.TransformDirection(Vector3.forward) * this.currentDistance;
+        this.transform.position = targetPos + Vector3.Normalize(this.transform.position - this.Target.position) * this.currentDistance;
 	}
 
-    //private float adjustLineOfSight(ref Vector3 targetPos, Vector3 direction)
-    //{
-    //    RaycastHit hit;
-    //    Debug.DrawRay(targetPos, direction * this.Distance, Color.yellow);
-    //    if (Physics.Raycast(targetPos, direction, out hit, this.Distance, this.LineOfSightMask.value))
-    //    {
-    //        float t = hit.distance - this.CloserRadius;
+    private float adjustLineOfSight(ref Vector3 targetPos, Vector3 direction)
+    {
+        RaycastHit hit;
+        Debug.DrawRay(targetPos, direction * this.Distance, Color.yellow);
+        if (Physics.Raycast(targetPos, direction, out hit, this.Distance, this.LineOfSightMask.value))
+        {
+            float t = hit.distance - this.CloserRadius;
 
-    //        targetPos += new Vector3(0, Mathf.Lerp(this.Height, 0, Mathf.Clamp(t, 0.0f, 1.0f)), 0);
-    //        return hit.distance - this.CloserRadius;
-    //    }
-    //    else
-    //    {
-    //        return hit.distance - this.CloserRadius;
-    //    }
+            targetPos += new Vector3(0, Mathf.Lerp(this.Height, 0, Mathf.Clamp(t, 0.0f, 1.0f)), 0);
+            return hit.distance - this.CloserRadius;
+        }
+        else
+        {
+            return this.Distance;
+        }
         
-    //}
+    }
 }
