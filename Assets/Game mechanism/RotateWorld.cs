4c8d0184895779a28456acd77d109b116bc7b548
Ian-Huang <dk781020@hotmail.com>
2012-07-17 18:20:38 +0800

    Refresh RotateWorld.cs & Update Begin scene

--- Assets/Game mechanism/RotateWorld.cs
+++ Assets/Game mechanism/RotateWorld.cs
@@ -5,11 +5,8 @@ public class RotateWorld : MonoBehaviour
 {
 
     public Vector3 Axis_v3;
-    public float speed = 1;
-    public bool isEnter = false;
     public bool isRotate = false;
     private GameObject Greta;
-    private Vector3 v3;
 
     void OnTriggerEnter(Collider other)
     {
@@ -19,8 +16,7 @@ public class RotateWorld : MonoBehaviour
             isRotate = true;
             Greta = m_parent;
             Greta.rigidbody.isKinematic = true;
-            v3 = Greta.transform.position;
-            RotateObject();
+            //RotateObject();
         }
     }
 
@@ -33,19 +29,13 @@ public class RotateWorld : MonoBehaviour
     void RotateObject()
     {
         transform.parent.transform.RotateAround(transform.position, Axis_v3, 90);
-        //Greta.transform.position += Greta.transform.TransformDirection(0, 5, 0);
         transform.parent.transform.position += transform.TransformDirection(new Vector3(0, 0, -1));
-        //if (Vector3.Equals(Physics.gravity, new Vector3(0.0f, -9.8f, 0.0f)))
-        //    Physics.gravity = new Vector3(0.0f, 0.0f, 9.8f);
-        //else
-        //    Physics.gravity = new Vector3(0.0f, -9.8f, 0.0f);
     }
 
-    float rotateFrame = 60;
-    int addFrame = 0;
-    public float deltaRotate;
-
-    float totalTime = 0.0f;
+    private float rotateFrame = 60;
+    private int addFrame = 0;
+    private float deltaRotate;
+    private float totalTime = 0.0f;
 
     //Update is called once per frame
     void Update()
@@ -59,27 +49,17 @@ public class RotateWorld : MonoBehaviour
                 this.totalTime = 0.05f;
             }
         }
-        //if (isRotate)
-        //{
-        //    if (addFrame < rotateFrame)
-        //    {
-        //        addFrame++;
-        //        transform.parent.transform.RotateAround(transform.position, Axis_v3, deltaRotate);
-        //        //transform.parent.transform.position += transform.TransformDirection(new Vector3(0, 0, -0.5f));
-        //        //Greta.rigidbody.isKinematic = true;
-        //        //Greta.rigidbody.velocity = Vector3.zero;
-        //        //print(Greta.rigidbody.velocity);
-        //    }
-        //    else
-        //    {
-        //        //Greta.rigidbody.isKinematic = false;
-        //        isRotate = false;
-        //    }
-        //}
-        //if (isEnter && !isRotate)
-        //    RotateObject();
-        //transform.RotateAround(Vector3.zero, v3, 20 * Time.deltaTime);
-        //transform.RotateAround(trans.position, Axis_v3, Time.deltaTime * speed);
-        //transform.RotateAroundLocal(Axis_v3, Time.deltaTime * speed);
+        if (isRotate)
+        {
+            if (addFrame < rotateFrame)
+            {
+                addFrame++;
+                transform.parent.transform.RotateAround(transform.position, Axis_v3, deltaRotate);
+            }
+            else
+            {
+                isRotate = false;
+            }
+        }
     }
 }
