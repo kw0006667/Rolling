96da5f56b10bed85dbce9b77b43caa12ff1bf502
Ian-Huang <dk781020@hotmail.com>
2012-07-19 13:30:24 +0800

    Modify DoorController & Add EndingMechanism

--- Assets/Game mechanism/DoorController.cs
+++ Assets/Game mechanism/DoorController.cs
@@ -3,26 +3,34 @@ using System.Collections;
 
 public class DoorController : MonoBehaviour
 {
+    private bool isOpen = false;
+    private GameObject Greta;
 
     void OnTriggerEnter(Collider other)
     {
         GameObject m_parent = other.transform.parent.gameObject;
         if (m_parent.CompareTag(GameDefinition.GetTagName(GameDefinition.Tag.Player)))
-            if (!this.animation.IsPlaying("OpenDoorAnimation"))
-                this.animation.PlayQueued("OpenDoorAnimation");
+        {
+            Greta = m_parent;            
+            if (!this.isOpen)
+            {
+                if (!this.animation.IsPlaying("OpenDoorAnimation"))
+                    this.animation.PlayQueued("OpenDoorAnimation");
+            }
+            isOpen = true;
+        }
     }
-
-    void OnTriggerExit(Collider other)
+    
+    void Update()
     {
-        GameObject m_parent = other.transform.parent.gameObject;
-        if (m_parent.CompareTag(GameDefinition.GetTagName(GameDefinition.Tag.Player)))
-            if (!this.animation.IsPlaying("CloseDoorAnimation"))
-                this.animation.PlayQueued("CloseDoorAnimation");
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-        
+        if (this.isOpen)
+        {
+            if (Vector3.Distance(this.Greta.transform.position, this.transform.position) > 4)
+            {
+                this.isOpen = false;
+                if (!this.animation.IsPlaying("CloseDoorAnimation"))
+                    this.animation.PlayQueued("CloseDoorAnimation");
+            }
+        }
     }
 }
---
+++ Assets/Game mechanism/EndingMechanism.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class EndingMechanism : MonoBehaviour 
+{
+    private CellManager cellManager;
+    public bool isOpen = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject m_parent = other.transform.parent.gameObject;
+        if (m_parent.CompareTag(GameDefinition.GetTagName(GameDefinition.Tag.Player)) && this.isOpen)
+        {
+            Application.LoadLevel("FirstStage");
+        }
+    }
+
+	// Use this for initialization
+    void Start()
+    {
+        this.cellManager = GameObject.Find("GearCollection").GetComponent<CellManager>();
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (this.cellManager.currentCount == this.cellManager.totalCount)
+        {
+            this.renderer.enabled = true;
+            this.isOpen = true;
+        }
+    }
+}
