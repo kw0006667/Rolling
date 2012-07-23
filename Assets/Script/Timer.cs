be1eb09ce6b23025e5db789b8e570a99270b096e
Ian-Huang <dk781020@hotmail.com>
2012-07-23 13:14:28 +0800

    Building 2012-07023-01

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -8,8 +8,14 @@ class Timer : MonoBehaviour
     
     private float t;
     private float s;
+    public GUIStyle style;
 
-    
+    public int hour;
+    public int min;
+    public float sec;
+
+    public string str;
+
     void Start()
     {
         s = Time.time;
@@ -19,15 +25,26 @@ class Timer : MonoBehaviour
     {
         t = Time.time - s;
 
-        times = t / 0.016f;
+        //times = t / 0.016f;
 
-        s = Time.time;
+        //s = Time.time;
+        //str = timer.hour.ToString("00") + ":" + timer.min.ToString("00") + ":" + timer.sec.ToString("00");
+        HandleTimer();
+    }
 
+    void HandleTimer()
+    {
+        hour = (int)t / 3600;
+        min = (int)((t - hour * 3600) / 60);
+        sec = (t - hour * 3600 - min * 60);
+        str = hour.ToString("00") + ":" + min.ToString("00") + ":" + sec.ToString("00.00");
     }
 
     void OnGUI()
     {
-        //GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), times.ToString() + " = " + t.ToString() + " / 0.016");
+        //GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), times.ToString() + " = " + t.ToString() + " / 0.016");e
+        GUI.TextField(new Rect(Screen.width-300, 100, 100, 100), str,style);
+
     }
 
 
