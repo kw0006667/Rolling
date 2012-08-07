fbb62cc970065875615f314a525bc7a6da8767f0
Ian-Huang <dk781020@hotmail.com>
2012-08-07 16:49:46 +0800

    Building 2012-08-07-01 about light

--- Assets/Script/DisplayFPS.cs
+++ Assets/Script/DisplayFPS.cs
@@ -12,34 +12,35 @@ public class DisplayFPS : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        if (!guiText)
-        {
-            print("FramesPerSecond needs a GUIText component!");
-            enabled = false;
-            return;
-        }
-        timeleft = updateInterval;
+        //if (!guiText)
+        //{
+        //    print("FramesPerSecond needs a GUIText component!");
+        //    enabled = false;
+        //    return;
+        //}
+        //timeleft = updateInterval;
         //guiText.transform.position = new Vector3(10.0f, Screen.height - 10, 10.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    timeleft -= Time.deltaTime;
-        accum += Time.timeScale/Time.deltaTime;
-        ++frames; 
+        Application.targetFrameRate = 70;
+        //timeleft -= Time.deltaTime;
+        //accum += Time.timeScale/Time.deltaTime;
+        //++frames; 
 
-        // Interval ended - update GUI text and start new interval
-        if( timeleft <= 0.0 )
-        {
-            // display two fractional digits (f2 format)
-            //guiText.text = "FPS = " + Time.timeScale.ToString("f2") + " / " + Time.deltaTime.ToString() + " = " + (accum/frames).ToString("f2");
-            timeleft = updateInterval;
-            accum = 0.0f;
-            frames = 0;
-        }
-        if (this.GetFPS() >= 70)
-            Application.targetFrameRate = 70;
+        //// Interval ended - update GUI text and start new interval
+        //if( timeleft <= 0.0 )
+        //{
+        //    // display two fractional digits (f2 format)
+        //    //guiText.text = "FPS = " + Time.timeScale.ToString("f2") + " / " + Time.deltaTime.ToString() + " = " + (accum/frames).ToString("f2");
+        //    timeleft = updateInterval;
+        //    accum = 0.0f;
+        //    frames = 0;
+        //}
+        //if (this.GetFPS() >= 70)
+        //    Application.targetFrameRate = 70;
 	}
 
     public float GetFPS()
